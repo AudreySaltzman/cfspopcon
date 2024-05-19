7d490827b3fdfbab5e4ba3c624aba951fc5452c3
Tom Body <tbody@cfs.energy>
2024-05-19 17:49:59 +0200

    Add flux consumption model from I. Savona

--- cfspopcon/formulas/geometry/analytical.py
+++ cfspopcon/formulas/geometry/analytical.py
@@ -7,7 +7,10 @@ from ...unit_handling import Unitfull
 
 @Algorithm.register_algorithm(return_keys=["plasma_volume"])
 def calc_plasma_volume(major_radius: Unitfull, inverse_aspect_ratio: Unitfull, areal_elongation: Unitfull) -> Unitfull:
-    """Calculate the plasma volume inside the last-closed-flux-surface.
+    """Calculate the plasma volume inside an up-down symmetrical last-closed-flux-surface.
+
+    Geometric formulas for system codes including the effect of negative triangularity :cite: `sauter`
+    NOTE: delta=1.0 is assumed since this was found to give a closer match to 2D equilibria from FreeGS.
 
     Args:
         major_radius: [m] :term:`glossary link<major_radius>`
@@ -42,6 +45,22 @@ def calc_plasma_surface_area(major_radius: Unitfull, inverse_aspect_ratio: Unitf
     return (
         2.0 * np.pi * (major_radius**2.0) * inverse_aspect_ratio * areal_elongation * (np.pi + 2.0 - (np.pi - 2.0) * inverse_aspect_ratio)
     )
+
+
+@Algorithm.register_algorithm(return_keys=["poloidal_circumference"])
+def calc_plasma_poloidal_circumference(minor_radius: Unitfull, areal_elongation: Unitfull) -> Unitfull:
+    """Calculate the plasma poloidal circumference at the last-closed-flux-surface.
+
+    Geometric formulas for system codes including the effect of negative triangularity :cite: `sauter`
+
+    Args:
+        minor_radius: [m] :term:`glossary link<minor_radius>`
+        areal_elongation: [~] :term:`glossary link<areal_elongation>`
+
+    Returns:
+        :term:`poloidal_circumference` [m]
+    """
+    return 2 * np.pi * minor_radius * (1 + 0.55 * (areal_elongation - 1))
 
 
 calc_areal_elongation_from_elongation_at_psi95 = Algorithm.from_single_function(
